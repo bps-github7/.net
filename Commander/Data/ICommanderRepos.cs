5e1877b1b8711c2f093035354cf261cac523cb12
Ben Sehnert <bps53@protonmail.com>
2020-11-25 18:54:41 -0500

    Had a messy ol' time updating to .net core sdk v5.0.0. The old project didnt recieve these changes well. So rather than spending a load of time trying to debug that, I started a new project and transfered the old assets in, redoing the package managment commands in a new csproj file.
    Finished this api, done with patch, put and delete.
    
    Since I need to move onto finishing
    up older portfolio projects and deploying
    things, I decided i will not develop a front
    end for this api, and likely not the spring one either. Best case is
    we finish up the react thing for Spring
    and use swagger ui to auto document this one.

---
+++ Commander/Data/ICommanderRepos.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public interface ICommanderRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand(Command cmd);
+        void updateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
+    }
+}
